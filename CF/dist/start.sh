5837896617a45c4c14d789df69480fa5917eb569
pakhandi <asimkprasad@gmail.com>
2015-05-27 04:46:26 -0700

    Enhancement

--- CF/dist/start.sh
+++ CF/dist/start.sh
@@ -8,5 +8,9 @@ fi
 chmod -R 777 $round
 subl ./$round
 subl $round/A/aprog.cpp
+
+#Uncomment the comments below to open the problemset in your default browser
+#Make sure you have xdg-open installed on your system...
+
 #alias xdg-open="xdg-open 2>/dev/null"
 #xdg-open http://codeforces.com/contest/$round/problems
