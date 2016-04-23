ef52aa2dcf0184c0693b10cc1dfea411e23fb006
pakhandi <asimkprasad@gmail.com>
2016-04-23 19:12:50 +0530

    Add getproxy

---
+++ src_linux/getProxy.py
@@ -0,0 +1,12 @@
+configFile = open('config', 'r')
+newDict = {}
+for line in configFile:
+	listedline = line.strip().split(':',1)
+	if len(listedline) > 1:
+		newDict[listedline[0]] = listedline[1]
+proxyConfig = newDict["proxyConfig"]
+
+proxyConfig = proxyConfig.lstrip(' ')
+proxyConfig = proxyConfig.rstrip(' ')
+
+print proxyConfig
