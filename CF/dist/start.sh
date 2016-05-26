69650e8351983a771de68d3c5254db01bb3222dc
pakhandi <asimkprasad@gmail.com>
2016-05-26 21:35:51 +0530

    Update start

--- CF/dist/start.sh
+++ CF/dist/start.sh
@@ -1,13 +1,22 @@
 echo "Enter the round number : "
 read round
-echo $round > temp.txt
+
+echo $round > temp
 ./ini $round
-if [ ! -d "$round" ]; then
+
+while read line           
+do            
+    echo $line
+    break         
+done <temp
+
+if [ ! -d "$line$round" ]; then
   exit
 fi
-chmod -R 777 $round
-subl ./$round
-subl $round/A/aprog.cpp
+
+chmod -R 777 $line$round
+subl $line$round
+subl $line$round/A/aprog.cpp
 
 #Uncomment the comments below to open the problemset in your default browser
 #Make sure you have xdg-open installed on your system...
