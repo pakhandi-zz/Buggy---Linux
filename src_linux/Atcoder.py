f61290636e42e7b16cc72286874d1994c0ba58f6
pakhandi <asimkprasad@gmail.com>
2017-05-16 23:04:00 +0530

    clean code

--- src_linux/Atcoder.py
+++ src_linux/Atcoder.py
@@ -1,69 +1,74 @@
 import requests
 from bs4 import BeautifulSoup
 import readConfig
+import sys
 
-import logging
+def parseProblem(sessionElement, problemUrl, problemIndex):
+	problemPage = sessionElement.get(problemUrl)
+	problemPageText = BeautifulSoup(problemPage.text)
 
-# These two lines enable debugging at httplib level (requests->urllib3->http.client)
-# You will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
-# The only thing missing will be the response.body which is not logged.
-try:
-    import http.client as http_client
-except ImportError:
-    # Python 2
-    import httplib as http_client
-http_client.HTTPConnection.debuglevel = 1
+	print "#"*20, problemIndex, "$"*20
 
-# You must initialize logging, otherwise you'll not see debug output.
-logging.basicConfig()
-logging.getLogger().setLevel(logging.DEBUG)
-requests_log = logging.getLogger("requests.packages.urllib3")
-requests_log.setLevel(logging.DEBUG)
-requests_log.propagate = True
+	inputCaseNumber = 0
+	outputCaseNumber = 0
 
-
-def parseProblem(x, problemUrl, problemIndex):
-	x = x.get(problemUrl)
-	soup = BeautifulSoup(x.text)
-
-	for divs in soup.findChildren('div', {"class": "part"}):
+	for divs in problemPageText.findChildren('div', {"class": "part"}):
 		for div in divs.findAll('section', {}):
 			children = div.findChildren('h3')
-
 			for child in children:
 				headerString = div.h3.string
-				# print "T"*5, "\n"
 				if ("Input" in headerString) and ( ("Sample" in headerString) or ("Example" in headerString) ):
-					print headerString
 					inputCase = div.pre.string
+					print "Case: ", inputCaseNumber
 					print inputCase
+					inputCaseNumber += 1
 				if ("Output" in headerString) and ( ("Sample" in headerString) or ("Example" in headerString) ):
-					print headerString
 					outputCase = div.pre.string
+					print "Case: ", outputCaseNumber
 					print outputCase
+					outputCaseNumber += 1
 
-def test(xx, contestUrl = 'https://abc061.contest.atcoder.jp/assignments'):
-	x = xx.get(contestUrl)
-	soup = BeautifulSoup(x.text)
+def parseContest(sessionElement, contestId):
+	contestUrl = getAllProblemsUrl(contestId)
+	print contestUrl
+	allProblemsPage = sessionElement.get(contestUrl)
+	allProblemsPageText = BeautifulSoup(allProblemsPage.text)
 	num = 0
-	for div in soup.findAll('tbody'):
-		num += 1
+	for div in allProblemsPageText.findAll('tbody'):
 		for row in div.findAll('tr'):
+			num += 1
 			cols = row.findAll('td')
-			print ">",cols[0]
-			ele = cols[0]
-			ele = ele.findAll('a')
-			problemRelativeUrl = ele[0]['href']
-			problemUrl = 'https://abc061.contest.atcoder.jp' + problemRelativeUrl
-			parseProblem(xx, problemUrl, num)
+			problemTag = cols[0]
+			problemLinkTag = problemTag.findAll('a')
+			problemRelativeUrl = problemLinkTag[0]['href']
+			problemUrl = getProblemUrl(contestId, problemRelativeUrl)
+			parseProblem(sessionElement, problemUrl, num)
 
-# Fill in your details here to be posted to the login form.
-payload = {
-	'name': readConfig.get("username"),
-	'password': readConfig.get("password")
-}
+def getContestUrl(contestId):
+	return "https://" + contestId + ".contest.atcoder.jp/"
 
-# Use 'with' to ensure the session context is closed after use.
-with requests.Session() as s:
-	p = s.post('https://practice.contest.atcoder.jp/login', data=payload)
-	test(s)
+def getAllProblemsUrl(contestId):
+	contestUrl = getContestUrl(contestId)
+	return contestUrl + "assignments/"
+
+def getProblemUrl(contestId, problemRelativeUrl):
+	contestUrl = getContestUrl(contestId)
+	return contestUrl + problemRelativeUrl
+
+def getLoginUrl(contestId):
+	contestUrl = getContestUrl(contestId)
+	return contestUrl + "login/"
+
+def doParsing(contestId):
+	payload = {
+		'name': readConfig.get("username"),
+		'password': readConfig.get("password")
+	}
+	
+	with requests.Session() as sessionElement:
+		retVal = sessionElement.post(getLoginUrl(contestId), data=payload)
+		parseContest(sessionElement, contestId)
+
+# call it
+contestId = sys.argv[1]
+doParsing(contestId)
