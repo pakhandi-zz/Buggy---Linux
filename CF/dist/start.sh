021f491fa9efdd20ecde726142ae05524ecb5e04
pakhandi <asimkprasad@gmail.com>
2017-05-18 23:27:15 +0530

    Add binary files

--- CF/dist/start.sh
+++ CF/dist/start.sh
@@ -1,22 +1,13 @@
 echo "Enter the round number : "
 read round
-
 echo $round > temp
 ./ini $round
-
-while read line           
-do            
-    echo $line
-    break         
-done <temp
-
-if [ ! -d "$line$round" ]; then
+if [ ! -d "$round" ]; then
   exit
 fi
-
-chmod -R 777 $line$round
-subl $line$round
-subl $line$round/A/aprog.cpp
+chmod -R 777 $round
+subl ./$round
+subl $round/A/aprog.cpp
 
 #Uncomment the comments below to open the problemset in your default browser
 #Make sure you have xdg-open installed on your system...
--- CF/dist/template.java
+++ CF/dist/template.java
@@ -14,7 +14,7 @@ import java.util.NoSuchElementException;
 import java.io.InputStream;
 
 
-public class aprog
+public class sol
 {
 	public static void main(String[] args)
 	{
