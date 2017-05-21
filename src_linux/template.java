ad1b20a615d23ee756f2f0dd8fc0a53f10ad5217
pakhandi <asimkprasad@gmail.com>
2017-05-21 18:41:19 +0530

    Shift scripts to src

---
+++ src_linux/template.java
@@ -0,0 +1,23 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.io.BufferedWriter;
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.util.NoSuchElementException;
+import java.io.InputStream;
+
+
+public class sol
+{
+	public static void main(String[] args)
+	{
+
+	}
+}
